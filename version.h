6476d2bdc27e1ec32ece377bd2182a04fd9eb3a2
Hiroki Motoyoshi <himotoyoshi@kde.biglobe.ne.jp>
2018-10-02 12:08:09 +0900

    tiny version up

--- version.h
+++ version.h
@@ -10,9 +10,9 @@
 
 ---------------------------------------------------------------------------- */
 
-#define CA_VERSION "1.3.0"
-#define CA_VERSION_CODE 130
+#define CA_VERSION "1.3.1"
+#define CA_VERSION_CODE 131
 #define CA_VERSION_MAJOR 1
 #define CA_VERSION_MINOR 3
-#define CA_VERSION_TEENY 0
+#define CA_VERSION_TEENY 1
 #define CA_VERSION_DATE "2017/11/18"
