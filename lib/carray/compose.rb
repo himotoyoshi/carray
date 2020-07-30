f38b8f6d418f2d493d1c0a46dcb54c633c6d1188
himotoyoshi <himotoyoshi@kde.biglobe.ne.jp>
2020-07-30 16:54:47 +0900

    Updated Copyright years in lib/**/*.rb

--- lib/carray/compose.rb
+++ lib/carray/compose.rb
@@ -3,10 +3,8 @@
 #  carray/composition.rb
 #
 #  This file is part of Ruby/CArray extension library.
-#  You can redistribute it and/or modify it under the terms of
-#  the Ruby Licence.
 #
-#  Copyright (C) 2005 Hiroki Motoyoshi
+#  Copyright (C) 2005-2020 Hiroki Motoyoshi
 #
 # ----------------------------------------------------------------------------
 
--- lib/carray/struct.rb
+++ lib/carray/struct.rb
@@ -3,19 +3,17 @@
 #  carray/struct.rb
 #
 #  This file is part of Ruby/CArray extension library.
-#  You can redistribute it and/or modify it under the terms of
-#  the Ruby Licence.
 #
-#  Copyright (C) 2005 Hiroki Motoyoshi
+#  Copyright (C) 2005-2020 Hiroki Motoyoshi
 #
 # ----------------------------------------------------------------------------
 #
 # The data class for fixed length carray are required to satisfy only
-# four conditions.
+# five conditions.
 #
 #   * constant data_class::DATA_SIZE    -> integer
-#   * constant data_class::MEMBERS      -> array of string
 #   * constant data_class::MEMBER_TABLE -> hash
+#   * constant data_class::MEMBERS      -> array (MEMBER_TABLE.keys as usual)
 #   * method   data_class.decode(data)  -> new data_class object
 #   * method   data_class#encode()      -> string 
 #
